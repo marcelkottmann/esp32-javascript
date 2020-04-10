c516678177bd842e12b3da229c9b2f5c900dba09
Marcel Kottmann <marcel.kottmann@sinnerschrader.com>
2020-04-10 08:02:08 +0200

    setup page refactored

--- components/esp32-javascript/modules/esp32-javascript/configserver.js
+++ components/esp32-javascript/modules/esp32-javascript/configserver.js
@@ -20,7 +20,7 @@ function page(res, headline, text, cb) {
     res.write(getHeader(200, 'Content-type: text/html\r\n'));
     res.write('<!doctype html><html><head><title>esp32-javascript</title>');
     res.write('<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">');
-    res.write('<link rel="stylesheet" href="/style.css.gz"></head>');
+    res.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pure/1.0.1/pure-min.css"></head>');
     res.write('<body><div class="pure-g"><div class="pure-u-1"><div class="l-box"><h1>');
     res.write(headline);
     res.write('</h1>');
@@ -41,13 +41,9 @@ function startConfigServer() {
             console.debug('401 response');
             res.write(getHeader(401, 'WWW-Authenticate: Basic realm="Enter credentials"\r\n'));
             res.end('401 Unauthorized');
-        } else if (req.path === '/restart') {
-            if (req.method === 'GET') {
-                page(res, 'Request restart', '<form action="/restart" method="post"><input type="submit" value="Restart" /></form>');
-            } else {
-                page(res, 'Restarting...', '', function () { setTimeout(restart, 1000) });
-            }
-        } else if (req.path === '/setup') {
+        } else if (req.path === '/restart' && req.method === 'POST'){
+            page(res, 'Restarting...<br /><a href="/">Home</a>', '', function () { setTimeout(restart, 1000) }); 
+        } else if (req.path === '/setup' || req.path === '/restart') {
             if (req.method === 'GET') {
                 page(res, 'Setup', ['<form  class="pure-form pure-form-stacked" action="/setup" method="post"><fieldset>',
                     '<label for="ssid">SSID</label><input type="text" name="ssid" value="', el_load('config.ssid'), '" />',
@@ -56,7 +52,9 @@ function startConfigServer() {
                     '<label for="offline" class="pure-checkbox"><input type="checkbox" name="offline" value="true" ',
                     (el_load('config.offline') === 'true' ? 'checked' : ''), '/> Offline Mode</label>',
                     '<textarea name="script">', el_load('config.script'), '</textarea>',
-                    '<input type="submit" class="pure-button pure-button-primary" value="Save" /></fieldset></form>']);
+                    '<input type="submit" class="pure-button pure-button-primary" value="Save" /></fieldset></form>',
+                '<h1>Request restart</h1>',
+                '<form action="/restart" method="post"><input type="submit" class="pure-button pure-button-primary" value="Restart" /></form>']);
             } else {
                 var config = http.parseQueryStr(req.body);
                 el_store('config.ssid', config.ssid);
